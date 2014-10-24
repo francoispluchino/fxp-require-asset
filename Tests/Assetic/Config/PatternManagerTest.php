478261035805a42bd7a87be467bfdbd576e3c1df
François Pluchino <francois.pluchino@gmail.com>
2014-10-24 18:12:01 +0200

    Add pattern manager tests

---
+++ Tests/Assetic/Config/PatternManagerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the Fxp Require Asset package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fxp\Component\RequireAsset\Tests\Assetic\Config;
+
+use Fxp\Component\RequireAsset\Assetic\Config\PatternManager;
+
+/**
+ * Pattern Manager Tests.
+ *
+ * @author François Pluchino <francois.pluchino@gmail.com>
+ */
+class PatternManagerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBasic()
+    {
+        $pm = new PatternManager();
+
+        $pm->addDefaultPattern('pattern1/*');
+        $pm->addDefaultPatterns(array(
+            'pattern2/*',
+            'pattern3/*',
+        ));
+
+        $this->assertTrue($pm->hasDefaultPattern('pattern1/*'));
+        $this->assertTrue($pm->hasDefaultPattern('pattern2/*'));
+        $this->assertTrue($pm->hasDefaultPattern('pattern3/*'));
+        $this->assertFalse($pm->hasDefaultPattern('nonexistingpattern'));
+
+        $pm->removeDefaultPattern('pattern2/*');
+
+        $this->assertTrue($pm->hasDefaultPattern('pattern1/*'));
+        $this->assertFalse($pm->hasDefaultPattern('pattern2/*'));
+        $this->assertTrue($pm->hasDefaultPattern('pattern3/*'));
+
+        $valid = array(
+            'pattern1/*',
+            'pattern3/*'
+        );
+        $this->assertSame($valid, $pm->getDefaultPatterns());
+    }
+
+    public function testAddDefaultPatternWithLockedManager()
+    {
+        $this->setExpectedException('Fxp\Component\RequireAsset\Exception\BadMethodCallException');
+
+        $pm = new PatternManager();
+        $this->assertSame(array(), $pm->getDefaultPatterns());
+
+        $pm->addDefaultPattern('pattern');
+    }
+
+    public function testAddDefaultPatternsWithLockedManager()
+    {
+        $this->setExpectedException('Fxp\Component\RequireAsset\Exception\BadMethodCallException');
+
+        $pm = new PatternManager();
+        $this->assertSame(array(), $pm->getDefaultPatterns());
+
+        $pm->addDefaultPatterns(array('pattern'));
+    }
+
+    public function testAddRemovePatternWithLockedManager()
+    {
+        $this->setExpectedException('Fxp\Component\RequireAsset\Exception\BadMethodCallException');
+
+        $pm = new PatternManager();
+        $this->assertSame(array(), $pm->getDefaultPatterns());
+
+        $pm->removeDefaultPattern('pattern');
+    }
+}
