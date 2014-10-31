50f117d4b93ec07b443052f6ef50d506d6853b64
François Pluchino <francois.pluchino@gmail.com>
2014-10-31 19:24:38 +0100

    Add utils file extension tests

---
+++ Tests/Assetic/Util/FileExtensionUtilsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Fxp Require Asset package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fxp\Component\RequireAsset\Tests\Assetic\Util;
+
+use Fxp\Component\RequireAsset\Assetic\Util\FileExtensionUtils;
+
+/**
+ * File Extension Utils Tests.
+ *
+ * @author François Pluchino <francois.pluchino@gmail.com>
+ */
+class FileExtensionUtilsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreateByConfig()
+    {
+        $name = 'fileextension';
+        $options = array();
+        $filters = array();
+        $extension = 'customname';
+        $debug = true;
+        $exclude = true;
+
+        $fe = FileExtensionUtils::createByConfig($name, $options, $filters, $extension, $debug, $exclude);
+
+        $this->assertInstanceOf('Fxp\Component\RequireAsset\Assetic\Config\FileExtensionInterface', $fe);
+        $this->assertSame($name, $fe->getName());
+        $this->assertSame($options, $fe->getOptions());
+        $this->assertSame($filters, $fe->getFilters());
+        $this->assertSame($extension, $fe->getOutputExtension());
+        $this->assertSame($debug, $fe->isDebug());
+        $this->assertSame($exclude, $fe->isExclude());
+    }
+
+    public function testGetDefaultConfigs()
+    {
+        $configs = FileExtensionUtils::getDefaultConfigs();
+
+        $this->assertTrue(is_array($configs));
+    }
+}
