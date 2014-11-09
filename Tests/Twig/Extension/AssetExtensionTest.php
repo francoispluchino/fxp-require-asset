edd4dbfead44ee752bb4ccb7d2e3979807bbcc90
François Pluchino <francois.pluchino@gmail.com>
2014-11-09 11:09:04 +0100

    Rename fixture tests

--- Tests/Twig/Extension/AssetExtensionTest.php
+++ Tests/Twig/Extension/AssetExtensionTest.php
@@ -37,9 +37,9 @@ class AssetExtensionTest extends \PHPUnit_Framework_TestCase
 
     public function testAssetStyles()
     {
-        $tpl = $this->getTemplate('asset_css.html.twig');
+        $tpl = $this->getTemplate('inline_style.html.twig');
         $content = $tpl->render(array());
-        $valid = file_get_contents(__DIR__.'/../../Fixtures/Resources/views/asset_css.valid.template');
+        $valid = file_get_contents(__DIR__.'/../../Fixtures/Resources/views/inline_style.valid.template');
         $valid = str_replace("\r", "", $valid);
 
         $this->assertEquals(mb_convert_encoding($valid, 'utf8'), $content);
@@ -47,9 +47,9 @@ class AssetExtensionTest extends \PHPUnit_Framework_TestCase
 
     public function testAssetScripts()
     {
-        $tpl = $this->getTemplate('asset_js.html.twig');
+        $tpl = $this->getTemplate('inline_script.html.twig');
         $content = $tpl->render(array());
-        $valid = file_get_contents(__DIR__.'/../../Fixtures/Resources/views/asset_js.valid.template');
+        $valid = file_get_contents(__DIR__.'/../../Fixtures/Resources/views/inline_script.valid.template');
         $valid = str_replace("\r", "", $valid);
 
         $this->assertEquals(mb_convert_encoding($valid, 'utf8'), $content);
@@ -72,31 +72,31 @@ class AssetExtensionTest extends \PHPUnit_Framework_TestCase
 
     public function testEmptyBody()
     {
-        $this->getTemplate('asset_empty_body.html.twig');
+        $this->getTemplate('inline_style_empty_body.html.twig');
     }
 
     public function testInvalidNameType()
     {
         $this->setExpectedException('Twig_Error_Syntax');
-        $this->getTemplate('asset_invalid_name_type.html.twig');
+        $this->getTemplate('inline_style_invalid_name_type.html.twig');
     }
 
     public function testInvalidAttributeName()
     {
         $this->setExpectedException('Twig_Error_Syntax');
-        $this->getTemplate('asset_invalid_attr_name.html.twig');
+        $this->getTemplate('inline_style_invalid_attr_name.html.twig');
     }
 
     public function testInvalidOperator()
     {
         $this->setExpectedException('Twig_Error_Syntax');
-        $this->getTemplate('asset_invalid_operator.html.twig');
+        $this->getTemplate('inline_style_invalid_operator.html.twig');
     }
 
     public function testInvalidValue()
     {
         $this->setExpectedException('Twig_Error_Syntax');
-        $this->getTemplate('asset_invalid_value.html.twig');
+        $this->getTemplate('inline_style_invalid_value.html.twig');
     }
 
     public function testTagPositionIsAlreadyIncluded()
